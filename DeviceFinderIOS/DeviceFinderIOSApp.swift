16fbd088ec7a3fbd67a68c93776f481c84931076
kitaya <kkitaya@nzigen.com>
2024-02-18 20:11:55 +0900

    Initial Commit

---
+++ DeviceFinderIOS/DeviceFinderIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceFinderIOSApp.swift
+//  DeviceFinderIOS
+//
+//  Created by KaitoKitaya on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct DeviceFinderIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
